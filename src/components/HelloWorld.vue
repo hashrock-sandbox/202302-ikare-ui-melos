454f672a4870723d5b2fd70e425c9dd3dd72c154
hashrock <hashrock@users.noreply.github.com>
2023-02-06 00:21:43 +0900

    delete

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,16 +6,19 @@ const items = merosu.split("/")
 export default {
   data() {
     return {
-      msg: "",
+      msg: [],
       items
     }
   },
   methods: {
     input(str) {
-      this.msg += str
+      this.msg.push(str)
     },
     clear() {
-      this.msg = ""
+      this.msg.splice(0, this.msg.length)
+    },
+    remove(index) {
+      this.msg.splice(index, 1)
     }
   }
 }
@@ -23,10 +26,12 @@ export default {
 
 <template>
   <div>
-
   </div>
-  <div style="">
-    {{ msg }}
+  <div>
+    <div class="area__badge" v-for="(m, idx) in msg">
+      {{ m }}
+      <button @click="remove(idx)" class="area__badge__delete">x</button>
+    </div>
   </div>
   <button @click="clear">クリア</button>
   <details>
@@ -42,6 +47,29 @@ export default {
   font-size: 2rem;
   border: 1px solid black;
   min-height: 5rem;
+}
+
+.area__badge {
+  font-size: 1rem;
+  display: inline-block;
+  border-radius: 0.5rem;
+  padding: 0.1em;
+  position: relative;
+}
+.area__badge:hover > .area__badge__delete {
+  display: inline-block;
+}
+.area__badge:hover {
+  background-color: aquamarine;
+}
+
+.area__badge__delete{
+  display: none;
+  font-size: 0.5rem;
+  position: absolute;
+  top: 0;
+  right: -1rem;
+  z-index: 1;
 }
 
 .read-the-docs {
